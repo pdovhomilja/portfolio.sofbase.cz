ac6b0fdd24284500cec6e422614a97300521aa15
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-17 21:41:35 +0100

    Responsive Homepage

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -9,9 +9,9 @@ function MainPage({}: Props) {
     <div className="h-screen flex flex-col">
       <div className="flex flex-row h-full w-full">
         <div className="w-1/3 bg-black text-white text-3xl pt-5">
-          <h1 className="pl-20"> Softbase s.r.o.</h1>
+          <h1 className="pl-5 md:pl-10 xl:pl-20"> Softbase s.r.o.</h1>
           <div className="flex flex-row h-full items-center">
-            <div className="absolute flex h-[60%] w-[90%]">
+            <div className="absolute flex h-[60%] w-full">
               <div className="w-1/2 my-auto">
                 <motion.img
                   src="https://www.freecodecamp.org/news/content/images/size/w2000/2021/05/web3-future-of-web.jpg"
@@ -25,9 +25,13 @@ function MainPage({}: Props) {
               </div>
               <div className="flex w-1/2 items-center">
                 <div className="flex flex-col -ml-28 pl-5 justify-center w-full h-2/3">
-                  <h1 className="text-6xl">We are SoftBase</h1>
-                  <h2 className="text-5xl">best Web3 agency in the world</h2>
-                  <h3 className="text-5xl text-gray-600">
+                  <h1 className="pl-16 text-xl md:text-6xl md:pl-6 xl:text-8xl xl:pl-0">
+                    We are SoftBase
+                  </h1>
+                  <h2 className="pl-20 text-sm mt-3 md:text-4xl md:pl-8 xl:text-6xl xl:pl-3">
+                    best Web3 agency in the world
+                  </h2>
+                  <h3 className="pl-16 text-sm mt-3 text-gray-600 md:text-4xl md:pl-6 xl:text-5xl xl:pl-0">
                     operating from Prague
                   </h3>
                   <p></p>
@@ -36,11 +40,13 @@ function MainPage({}: Props) {
             </div>
           </div>
         </div>
-        <div className="flex flex-col w-2/3 bg-gradient-to-r from-gray-800 to-stone-900">
-          <div className="flex flex-row w-full h-24 justify-end">
-            <p className="text-gray-300 pt-10 px-5">Menu1</p>
-            <p className="text-gray-300 pt-10 px-5">Menu 2</p>
-            <p className="text-gray-300 pt-10 px-5">Menu 3</p>
+        <div className="w-2/3 bg-gradient-to-r from-gray-800 to-stone-900">
+          <div className="md:flex md:flex-row w-full h-24 justify-end">
+            <p className="text-gray-300 pt-10 px-5 bg-black hover:bg-blue-700">
+              Home
+            </p>
+            <p className="text-gray-300 pt-10 px-5 hover:bg-blue-700">Menu 2</p>
+            <p className="text-gray-300 pt-10 px-5 hover:bg-blue-700">Menu 3</p>
             <p className="text-gray-300 pt-10 px-5 hover:bg-blue-700">Menu 4</p>
             <p className="text-gray-300 pt-10 pl-5 pr-24 hover:bg-blue-700">
               Menu 5
