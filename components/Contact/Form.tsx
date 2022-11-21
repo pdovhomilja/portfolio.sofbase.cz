45357df1d91a47db68a81bde542cbc7a3b97daf7
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-21 01:28:32 +0100

    Add web form functionality

--- components/Contact/Form.tsx
+++ components/Contact/Form.tsx
@@ -1,17 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {};
 
 function Form({}: Props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    console.log("Sending");
+    let data = {
+      name,
+      email,
+      message,
+    };
+    console.log(data);
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  };
   return (
     <form action="" className="p-5">
       <p className="font-bold text-xl text-white pb-2">Name</p>
-      <input type="text" className="rounded-md w-full pr-6 bg-gray-500" />
+      <input
+        type="text"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        name="name"
+        className="rounded-md w-full pr-6 bg-gray-500"
+      />
       <p className="font-bold text-white pb-2">E-mail</p>
-      <input type="text" className="rounded-md w-full pr-6 bg-gray-500" />
+      <input
+        type="text"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        name="email"
+        className="rounded-md w-full pr-6 bg-gray-500"
+      />
       <p className="font-bold text-white pb-2">Message</p>
-      <textarea className="rounded-md w-full pr-6 bg-gray-500" />
-      <button className="bg-[#00e893] p-3 mt-3 font-bold text-[#1f003d] rounded-md">
+      <textarea
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+        name="message"
+        className="rounded-md w-full pr-6 bg-gray-500"
+      />
+      <button
+        onClick={(e) => {
+          handleSubmit(e);
+        }}
+        className="bg-[#00e893] p-3 mt-3 font-bold text-[#1f003d] rounded-md"
+      >
         Click to enter web3 world
       </button>
     </form>
