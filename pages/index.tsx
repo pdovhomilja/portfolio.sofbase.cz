083d03896eb00042d9297c7ec0ccc4f261262a4b
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-23 16:15:15 +0100

    add meta content tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,13 @@ export default function Home() {
     <>
       <Head>
         <title>SoftBase - Web3 agency</title>
-        <meta name="description" content="" />
+        <meta
+          name="description"
+          content="We are SoftBase
+Web3 agency operating from Prague
+with strong business & technical skills
+We bring incredible things in Web3 space - Blockchain, Lens protocol, NFT, smart contract. All topics we love and successfully manage projects for our clients"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="h-screen snap-y snap-proximity overflow-auto bg-black">
