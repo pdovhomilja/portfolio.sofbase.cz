5fdcae85a1ff7cfa132ca4e6b1e99726a4fe2ce7
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-23 16:59:55 +0100

    Update contacts

--- components/Contact/Contacts.tsx
+++ components/Contact/Contacts.tsx
@@ -3,6 +3,8 @@ import {
   EnvelopeIcon,
   MapPinIcon,
   PhoneIcon,
+  CurrencyEuroIcon,
+  BuildingOfficeIcon,
 } from "@heroicons/react/24/outline";
 
 type Props = {};
@@ -34,6 +36,25 @@ function Contacts({}: Props) {
         </div>
         <div className="mt-2">
           <p className="font-bold">info@softbase.cz</p>
+        </div>
+      </div>
+      <div className="flex my-2">
+        <div className="mr-2">
+          <CurrencyEuroIcon className="h-6 w-6 text-gray-500 mr-1" />
+        </div>
+        <div className="mt-2">
+          <p className="font-bold">Fio Bank</p>
+          <p>2902352127/2010</p>
+          <p>IBAN:CZ1620100000002902352127</p>
+          <p>SWIFT/BIC:FIOBCZPPXXX</p>
+        </div>
+      </div>
+      <div className="flex my-2">
+        <div className="mr-2">
+          <BuildingOfficeIcon className="h-6 w-6 text-gray-500 mr-1" />
+        </div>
+        <div className="mt-2">
+          <p className="font-bold">VAT:17197031</p>
         </div>
       </div>
     </>
